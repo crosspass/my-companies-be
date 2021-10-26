6edaca390416175c2b6c31461834878fbd097347
iiithinking <wuyingchao1@gmail.com>
2021-10-26 10:40:48 +0800

    Fix: csv chart update

--- src/components/csv_form.tsx
+++ src/components/csv_form.tsx
@@ -1,113 +1,122 @@
-import React, { useState } from 'react'
-import { Popover, Button, Col, Menu, Input, InputNumber, Radio, Row, Space, Tooltip } from "antd"
-import { AppstoreOutlined } from '@ant-design/icons'
-import ReactDataSheet from 'react-datasheet'
-import "react-datasheet/lib/react-datasheet.css";
-import "@/pages/custom.css";
+import React, { useEffect, useState } from 'react';
+import {
+  Popover,
+  Button,
+  Col,
+  Menu,
+  Input,
+  InputNumber,
+  Radio,
+  Row,
+  Space,
+  Tooltip,
+} from 'antd';
+import { AppstoreOutlined } from '@ant-design/icons';
+import ReactDataSheet from 'react-datasheet';
+import 'react-datasheet/lib/react-datasheet.css';
+import '@/pages/custom.css';
 
-import CSVChart2 from "@/components/dcvs"
-import styles from "@/pages/index.less"
+import CSVChart2 from '@/components/dcvs';
+import styles from '@/pages/index.less';
 
 export interface GridElement extends ReactDataSheet.Cell<GridElement, number> {
   value: number | null;
 }
 
-class MyReactDataSheet extends ReactDataSheet<GridElement, number> { }
+class MyReactDataSheet extends ReactDataSheet<GridElement, number> {}
 
 interface AppState {
   grid: GridElement[][];
 }
 
 //You can also strongly type all the Components or SFCs that you pass into ReactDataSheet.
-let cellRenderer: ReactDataSheet.CellRenderer<GridElement, number> = (props) => {
-  const backgroundStyle = props.cell.value && props.cell.value < 0 ? { color: 'red' } : undefined;
+let cellRenderer: ReactDataSheet.CellRenderer<GridElement, number> = (
+  props,
+) => {
+  const backgroundStyle =
+    props.cell.value && props.cell.value < 0 ? { color: 'red' } : undefined;
   return (
-    <td style={backgroundStyle} onMouseDown={props.onMouseDown} onMouseOver={props.onMouseOver} onDoubleClick={props.onDoubleClick} className="cell">
+    <td
+      style={backgroundStyle}
+      onMouseDown={props.onMouseDown}
+      onMouseOver={props.onMouseOver}
+      onDoubleClick={props.onDoubleClick}
+      className="cell"
+    >
       {props.children}
     </td>
-  )
-}
-const CsvForm: React.FC = ({
-  initValue,
-  onChange,
-}) => {
-  let grid = initValue.data
-  if (typeof(initValue.data) === 'string') {
-    grid = JSON.parse(initValue.data)
-  } 
-  console.log("......grid", grid)
-  const chartType = initValue.chartType
-  const title = initValue.title
-  const valueRenderer = cell => cell.value;
-  const onCellsChanged = changes => {
-    const cgrid = grid.map(row => [...row])
-    changes.forEach(({ cell, row, col, value }) => {
-      cgrid[row][col] = { ...grid[row][col], value }
-    })
-    onChange({
-      id: initValue.id,
-      chartType: chartType,
-      title: title,
-      data: cgrid
-    })
-  };
-  const onContextMenu = (e, cell, i, j) => cell.readOnly ? e.preventDefault() : null;
-  const setChartType = e => {
-    onChange({
-      id: initValue.id,
-      title: title,
-      chartType: e.target.value,
-      data: grid
-    })
+  );
+};
+const CsvForm: React.FC = ({ initValue, onChange }) => {
+  let grid = initValue.data;
+  if (typeof initValue.data === 'string') {
+    grid = JSON.parse(initValue.data);
   }
+  console.log('......grid', grid);
+  const { chartType, title } = { ...initValue };
+  const valueRenderer = (cell) => cell.value;
+  const onCellsChanged = (changes) => {
+    const cgrid = grid.map((row) => [...row]);
+    changes.forEach(({ cell, row, col, value }) => {
+      cgrid[row][col] = { ...grid[row][col], value };
+    });
+    onChange({
+      data: cgrid,
+    });
+  };
+  const setChartType = (e) => {
+    onChange({
+      chartType: e.target.value,
+    });
+  };
   const setTitle = (e) => {
     onChange({
-      id: initValue.id,
-      chartType: chartType,
       title: e.target.value,
-      data: grid
-    })
-  }
+    });
+  };
   const [x, setX] = React.useState(grid.length);
   const [y, setY] = React.useState(grid[0].length);
   const [popVisible, setPopVisible] = React.useState(false);
   const hide = () => {
-    setPopVisible(false)
-  }
+    setPopVisible(false);
+  };
+  useEffect(() => {
+    setX(grid.length);
+    setY(grid[0].length);
+  }, [initValue]);
+
   const confirmXY = () => {
-    const newData = []
+    const newData = [];
     for (let i = 0; i < x; i++) {
-      const row = Array(y).fill({ value: '' }, 0, y)
-      const originRow = grid[i]
+      const row = Array(y).fill({ value: '' }, 0, y);
+      const originRow = grid[i];
       if (originRow) {
         for (let j = 0; j < y && j < originRow.length; j++) {
-          row[j] = originRow[j]
+          row[j] = originRow[j];
         }
       }
-      newData.push(row)
+      newData.push(row);
     }
     onChange({
-      id: initValue.id,
-      chartType: chartType,
-      title: title,
-      data: newData
-    })
-    hide()
-  }
+      data: newData,
+    });
+    hide();
+  };
 
   const menu = (
     <Menu>
       <Menu.Item>
         <Space>
-          <InputNumber value={x} onChange={v => setX(v)}></InputNumber>
+          <InputNumber value={x} onChange={(v) => setX(v)}></InputNumber>
           <span>x</span>
-          <InputNumber value={y} onChange={v => setY(v)}></InputNumber>
-          <Button type="primary" onClick={confirmXY}>确定</Button>
+          <InputNumber value={y} onChange={(v) => setY(v)}></InputNumber>
+          <Button type="primary" onClick={confirmXY}>
+            确定
+          </Button>
         </Space>
       </Menu.Item>
     </Menu>
   );
-
 
   return (
     <div>
@@ -132,7 +141,7 @@ const CsvForm: React.FC = ({
           </Radio.Group>
         </Col>
       </Row>
-      <Tooltip placement="topLeft" title='调整表格'>
+      <Tooltip placement="topLeft" title="调整表格">
         <Popover
           content={menu}
           title="调整表格"
@@ -141,11 +150,13 @@ const CsvForm: React.FC = ({
           onVisibleChange={setPopVisible}
           placement="bottomRight"
         >
-          <AppstoreOutlined className={styles.editableIcon} onClick={() => console.log("abcdef")} />
+          <AppstoreOutlined
+            className={styles.editableIcon}
+            onClick={() => console.log('abcdef')}
+          />
         </Popover>
       </Tooltip>
-      <div>
-      </div>
+      <div></div>
       <MyReactDataSheet
         data={grid}
         valueRenderer={valueRenderer}
@@ -155,4 +166,4 @@ const CsvForm: React.FC = ({
   );
 };
 
-export default CsvForm
+export default CsvForm;
--- src/pages/csvs.tsx
+++ src/pages/csvs.tsx
@@ -1,14 +1,35 @@
-import React, { useState, useReducer } from 'react'
-import { Popover, Button, Col, Modal, Card, Dropdown, Menu, Input, InputNumber, message, List, Radio, Row, Space, Tooltip, Upload } from "antd"
-import { AppstoreOutlined, PlusOutlined, UploadOutlined } from '@ant-design/icons'
-import { Link, connect } from "umi"
-import "@/pages/custom.css";
+import React, { useState, useReducer } from 'react';
+import {
+  Popover,
+  Button,
+  Col,
+  Modal,
+  Card,
+  Dropdown,
+  Menu,
+  Input,
+  InputNumber,
+  message,
+  List,
+  Radio,
+  Row,
+  Space,
+  Tooltip,
+  Upload,
+} from 'antd';
+import {
+  AppstoreOutlined,
+  PlusOutlined,
+  UploadOutlined,
+} from '@ant-design/icons';
+import { Link, connect } from 'umi';
+import '@/pages/custom.css';
 
-import CSVChart2 from "@/components/dcvs"
-import CsvForm from "@/components/csv_form"
-import { dateString } from "@/utils/dates"
+import CSVChart2 from '@/components/dcvs';
+import CsvForm from '@/components/csv_form';
+import { dateString } from '@/utils/dates';
 
-import styles from "@/pages/index.less"
+import styles from '@/pages/index.less';
 
 function page({ csvs, dispatch }) {
   const [confirmLoading, setConfirmLoading] = React.useState(false);
@@ -17,15 +38,11 @@ function page({ csvs, dispatch }) {
     title: '图表主题',
     chartType: 'line',
     data: [
-      [
-        { readOnly: true, value: '' },
-        { value: '数据1' },
-        { value: '数据2' },
-      ],
-      [{ value: "2021.09" }, { value: 1 }, { value: 2 }],
-      [{ value: "2021.10" }, { value: 1 }, { value: 4 }],
-    ]
-  }
+      [{ readOnly: true, value: '' }, { value: '数据1' }, { value: '数据2' }],
+      [{ value: '2021.09' }, { value: 1 }, { value: 2 }],
+      [{ value: '2021.10' }, { value: 1 }, { value: 4 }],
+    ],
+  };
 
   const showModal = () => {
     setState({ visible: true, data: cData });
@@ -33,27 +50,34 @@ function page({ csvs, dispatch }) {
 
   const [state, setState] = useReducer(
     (state, newState) => ({ ...state, ...newState }),
-    { visible: false, data: cData }
-  )
+    { visible: false, data: cData },
+  );
 
-  const onChange = (data: { title?: string, chartType?: string, grid?: Array<any> }) => {
-    setState({ data: data })
-  }
+  const onChange = (data: {
+    title?: string;
+    chartType?: string;
+    grid?: Array<any>;
+  }) => {
+    const newData = { ...state.data, ...data };
+    setState({ data: newData });
+  };
 
   const handleOk = async () => {
     setConfirmLoading(true);
-    const data = JSON.stringify(state.data.data)
-    console.log("data..............", state.data)
+    let data = state.data.data;
+    if (typeof state.data.data == 'object') {
+      data = JSON.stringify(state.data.data);
+    }
     if (state.data.id) {
       await dispatch({
         type: 'csvs/update',
         payload: { code: csvs.code, ...state.data, data },
-      })
+      });
     } else {
       await dispatch({
         type: 'csvs/create',
         payload: { code: csvs.code, ...state.data, data },
-      })
+      });
     }
     setState({ visible: false });
     setConfirmLoading(false);
@@ -68,13 +92,13 @@ function page({ csvs, dispatch }) {
       id: csv.ID,
       title: csv.title,
       chartType: csv.chartType,
-      data: csv.data
-    }
+      data: csv.data,
+    };
     setState({
       visible: true,
-      data: data
+      data: data,
     });
-  }
+  };
   const csvCard = function (csv) {
     return (
       <Card className={styles.articleCard}>
@@ -84,8 +108,8 @@ function page({ csvs, dispatch }) {
           <Button onClick={() => setUpdateCsv(csv)}>更新图表数据</Button>
         </Space>
       </Card>
-    )
-  }
+    );
+  };
   return (
     <div className={styles.mainContainer}>
       <h1>收集的数据</h1>
@@ -95,9 +119,14 @@ function page({ csvs, dispatch }) {
         size="large"
         dataSource={csvs.list}
         renderItem={csvCard}
-      >
-      </List>
-      <Button className={styles.addBtn} type="primary" shape="circle" icon={<PlusOutlined />} onClick={showModal} />
+      ></List>
+      <Button
+        className={styles.addBtn}
+        type="primary"
+        shape="circle"
+        icon={<PlusOutlined />}
+        onClick={showModal}
+      />
       <Modal
         title="更新图表数据"
         visible={state.visible}
@@ -109,14 +138,13 @@ function page({ csvs, dispatch }) {
         <CsvForm initValue={state.data} onChange={onChange} />
       </Modal>
     </div>
-  )
+  );
 }
 
-
 function mapStateToProps(state) {
-  console.log("csvs.code", state.csvs.code)
+  console.log('csvs.code', state.csvs.code);
   return {
-    csvs: state.csvs
+    csvs: state.csvs,
   };
 }
 
